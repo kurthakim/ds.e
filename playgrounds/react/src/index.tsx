5f57062aea8fa33b348042f0469b07734826dcc0
Kurt Hakim <kurthakim@gmail.com>
2022-01-11 00:18:31 +0700

    develop color react component

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Color } from '@ds.e/react';
+
+import '@ds.e/scss/lib/Color.css';
+
+ReactDOM.render(
+  <Color hexCode="#000" width="1rem" height="1rem" />,
+  document.querySelector('#root')
+);
